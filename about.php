fbb6106df9dec4d5e22e999723ddfa91f5d55215
tushar-26 <savdiyatushar17@gmail.com>
2025-01-18 13:31:43 +0530

    moved

---
+++ about.php
@@ -0,0 +1,161 @@
+  <?php
+session_start();
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+
+	    <!-- Css Styles -->
+          <meta charset="UTF-8">
+    <meta name="description" content="Male_Fashion Template">
+    <meta name="keywords" content="Male_Fashion, unica, creative, html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/jpg" href="img/logo.jpg">
+    <title>About Page</title>
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700;800;900&display=swap"
+        rel="stylesheet">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="css/elegant-icons.css" type="text/css">
+    <link rel="stylesheet" href="css/magnific-popup.css" type="text/css">
+    <link rel="stylesheet" href="css/nice-select.css" type="text/css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="css/slicknav.min.css" type="  text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">   
+    
+</head>
+<body>
+<?php
+include("header.php");
+?>
+<section id="aboutUs">
+  <div class="container">
+    <div class="heading text-left"> 
+      <!-- Heading -->
+      <div>
+      	<h2><u>About Us</u></h2>
+      </div>
+      <div class="wel_line"><center><h3>WELCOME</h3></center></div>
+    </div>
+    <div class="row feature design">
+
+      <div class="area1 columns left"><br><br>
+        
+        <h5>Welcome to EntertainEase, your ultimate gateway to hassle-free and effortless entertainment!</h5>
+        <p>At EntertainEase, we bring the joy of cinema closer to you, making it easier than ever to book tickets for the latest blockbusters at your favorite theaters. With a sleek, user-friendly interface and secure payment options, we ensure a seamless booking experience every time.
+        We’re dedicated to keeping movie lovers up-to-date with the newest releases, showtimes, and exclusive offers. Whether it’s a solo movie night or a fun outing with friends and family, EntertainEase makes planning your cinema experience simple and stress-free.
+        Your love for movies drives us to continuously improve and provide the best service possible. Choose EntertainEase and let us help you create unforgettable movie memories, one ticket at a time!
+        <br>
+    It is available for 24 X 7 hours.  </p> 
+
+      </div>
+      <div class="area2 columns feature-media left"> <img src="img/logo.jpg" alt="" width="100%"> </div>
+    </div>
+  
+    </div>
+  
+</section>
+<div class="dvCapacity">
+
+                                    <div class="CapacityHead">
+                                        SERVING HAPPINESS
+                       
+                       
+                                    </div>
+                                    <div class="CapHorizon">
+                                    </div>
+                                    <div class="PatronStruc">
+                                        <div class="row">
+
+
+
+                                            <div class="col-md-4">
+
+                                                <div class="dvPatron">
+                                                    <img src="image/unnamed.jpg" class="resize" alt="#" />
+
+                                                </div>
+
+                                                <div class="dvPatConetnt">
+                                                    <div class="dvPatronDesc">
+                                                        <div class="dvPatronDescHead">
+                                                            Luxury
+
+                                                        </div>
+                                                        <div class="dvPatronDescText">
+                                                           using entertainease you can book any movie ticket with luxury seat.
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                            </div>
+                                            <div class="col-md-4">
+
+                                                <div class="dvPatron">
+                                                    <img src="image/food_epicurious1-1-1d876222fa3d4f92.jpg" alt="#" />
+
+                                                </div>
+                                                <div class="dvPatConetnt">
+                                                    <div class="dvPatronDesc">
+                                                        <div class="dvPatronDescHead">
+                                                            Service
+                                                        </div>
+                                                        <div class="dvPatronDescText">
+                                                            Weekend popcorn,cold drinks,snacks and 'masala' on screen.We are committed to provide our guests with an unparalleled cinema journey, which is underlined by the warmth of our top notch service 
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                            </div>
+
+                                            <div class="col-md-4">
+
+                                                <div class="dvPatron">
+                                                    <img src="image/capacity-technology.jpg" class="resize" alt="#" />
+
+                                                </div>
+                                                <div class="dvPatConetnt">
+                                                    <div class="dvPatronDesc">
+                                                        <div class="dvPatronDescHead">
+                                                            Technology
+                                                        </div>
+                                                        <div class="dvPatronDescText">
+                                                            Be it projection &
+ sound or our advanced cinema formats or reservations, check-ins & food ordering, we aim to amplify your experience with the finest use of technology
+
+
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                            </div>
+                                        </div>
+
+                                    </div>
+
+
+                                </div>
+<?php
+include("footer.php");
+?>
+
+
+    <!-- Js Plugins -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.nice-select.min.js"></script>
+    <script src="js/jquery.nicescroll.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/jquery.countdown.min.js"></script>
+    <script src="js/jquery.slicknav.js"></script>
+    <script src="js/mixitup.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>     
---
+++ contact.php
@@ -0,0 +1,93 @@
+  <?php
+session_start();
+?>
+
+
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Male_Fashion Template">
+    <meta name="keywords" content="Male_Fashion, unica, creative, html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/jpg" href="img/logo.jpg">
+    <title>Contact Page</title>
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700;800;900&display=swap"
+        rel="stylesheet">
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/fontawesome.min.css">
+
+    <!-- Css Styles -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="css/elegant-icons.css" type="text/css">
+    <link rel="stylesheet" href="css/magnific-popup.css" type="text/css">
+    <link rel="stylesheet" href="css/nice-select.css" type="text/css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+
+<body>
+    <?php
+    include("header.php");
+    ?>
+
+    <!-- Contact Section Begin -->
+    <section class="contact spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-6">
+                    <div class="contact__text">
+                        <div class="section-title">
+                            <span>Information</span>
+                            <h2>Contact Us</h2>
+                            <p>Got questions or need assistance? We’re here to help! At EntertainEase, we value our customers and are committed to providing prompt support for all your movie ticket booking needs. Whether you have inquiries, face any issues, or just want to share your thoughts, don’t hesitate to reach out.
+
+You can contact us through our email or by providing feedback, and our dedicated support team will ensure your concerns are addressed as quickly as possible. Thank you for choosing EntertainEase — we’re just a message away!
+
+
+
+</p>
+</div>
+                        <ul>
+                            <li>
+                                <h4>Regards,</h4>
+                                <p><span style="color: red; font-weight: bold; margin-right:5px;">Tushar,</span> Vats, Kirtan, Darshita, Tejal, and Khushi
+                                <br /><span style="color:navy; font-weight: bold;">savdiyatushar17@gmail.com</span><br>
+                                vatspatel45@gmail.com<br />
+                                kirtan656@gmail.com</p>
+                            </li>
+                        
+                        </ul>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </section>
+    <!-- Contact Section End -->
+
+    <!-- Footer Section Begin -->
+<?php
+include("footer.php");
+?>
+
+    <!-- Js Plugins -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.nice-select.min.js"></script>
+    <script src="js/jquery.nicescroll.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/jquery.countdown.min.js"></script>
+    <script src="js/jquery.slicknav.js"></script>
+    <script src="js/mixitup.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
